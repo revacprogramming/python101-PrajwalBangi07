395dd7299e58dba2374a83243776c33615e3c9b1
PrajwalBangi07 <97020307+PrajwalBangi07@users.noreply.github.com>
2022-05-14 15:20:06 +0000

    py10

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -7,7 +7,8 @@ for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
     average += float(line[20:-1].strip())
     count = count + 1
-    #print(line)
+    print(line)
     
 print("Average spam confidence:", (average/count))
 
+SZ
--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,17 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line.rstrip()
+    t = line.split()
+    lst.append(t)
+
+lst1 = []
+for i in lst:
+    lst1 = lst1+ i
+lst1 = list(set(lst1))
+lst1.sort()        
+print(lst1)
